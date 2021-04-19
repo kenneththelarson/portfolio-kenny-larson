cb2f98f5e6e90b3ac55a7e7ab73e0d84bd062c81
Kenny <kenneththelarson@gmail.com>
2021-04-18 23:37:05 -0600

    projects setup

---
+++ src/components/Projects/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import miles from '../../assets/portfolio/miles-ahead-logo.png';
+import blog from '../../assets/portfolio/the_tech_blog_screenshot.png';
+import fiesta from '../../assets/portfolio/urban-fiesta.jpg';
+import weather from '../../assets/portfolio/WeatherScreenshot.png';
+import quiz from '../../assets/portfolio/javascriptQuizScreenshot';
+import scheduler from '../../assets/portfolio/workDaySchedulerScreenshot.png';
+
+
+function Projects() {
+    return (
+        <section>
+            <h2>Miles Ahead</h2>
+            <p>A fitness tracking app</p>
+        </section>
+    );
+}
+
+export default Projects;
