c0f9dc14f96051edf601653466e5574cf7204134
Kenny <kenneththelarson@gmail.com>
2021-04-20 21:27:14 -0600

    contact and resume components added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import Nav from './components/Nav';
 import About from './components/About';
 import Portfolio from './components/Portfolio';
+import Contact from './components/Contact';
+import Resume from './components/Resume';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
@@ -14,6 +16,8 @@ function App() {
           <Switch>
             <Route exact path="/About" component={About} />
             <Route exact path="/Portfolio" component={Portfolio} />
+            <Route exact path="/Contact" component={Contact} />
+            <Route exact path="/Resume" component={Resume} />
           </Switch>
         </main>
       </div>
---
+++ src/components/Resume/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Resume() {
+    return (
+        <div>
+            <p>Click me, please!</p>
+            <div>
+                <a href="./assets/resume" download type="application/pdf"></a>
+            </div>
+        </div>
+    )
+}
+
+export default Resume;
